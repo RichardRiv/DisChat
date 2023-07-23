e0cf5cee5df4668e5cc646de1a579d62f9e2f2b3
RichardRiv <r.richardev@gmail.com>
2023-07-22 20:09:45 -0400

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './pages/SignIn';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return <SignIn />;
+
+	// TODO: If User, take to main page, otherewise signIn page
+};
 
 export default App;
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,14 @@
+import './styles.scss';
+
+const SignIn = () => {
+	return (
+		<div className='bg'>
+			<div className='container'>
+                <p className='header'>Welcome Back!</p>
+                <p className='sub-header'>We are so excited to see you again!</p>
+            </div>
+		</div>
+	);
+};
+
+export default SignIn;
